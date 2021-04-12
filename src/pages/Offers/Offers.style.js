47b6a83b6924f01eaf19d1bec12cf8806aecf24a
ubirajarapelli <ubirajaracpelli@gmail.com>
2021-04-12 11:22:37 -0300

    Add media querie

--- src/pages/Offers/Offers.style.js
+++ src/pages/Offers/Offers.style.js
@@ -2,8 +2,15 @@ import styled from "styled-components";
 
 export const OffersContainer = styled.div`
   display: grid;
-  grid-template-columns: 20% 75%;
+  
   margin-top: 24px;
+  @media (min-width: 1349px) {
+    grid-template-columns: 15% 75%;
+  }
+
+  @media (min-width: 1400px) {
+    grid-template-columns: 20% 75%;
+  }
 `;
 
 export const Filters = styled.div``;
